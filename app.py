7d8237db961192a61600f1bc146016699f2c6c94
Diago Estrada <estradadiago93@gmail.com>
2023-04-21 11:39:51 -0400

    Configure app for multiple environments using a single aws account

--- app.py
+++ app.py
@@ -1,28 +1,15 @@
 #!/usr/bin/env python3
-import os
-
 import aws_cdk as cdk
 
 from metrics_receiver_cdk.metrics_receiver_cdk_stack import MetricsReceiverCdkStack
 
 
 app = cdk.App()
-MetricsReceiverCdkStack(app, "MetricsReceiverCdkStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
+prefix = app.node.try_get_context("prefix")
+environments = app.node.try_get_context("environments")
 
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+for environment, configuration in environments.items():
+    construct_id = f"{prefix}CdkStack{environment.capitalize()}"
+    MetricsReceiverCdkStack(app, construct_id, prefix=prefix, environment=environment, config=configuration)
 
 app.synth()
