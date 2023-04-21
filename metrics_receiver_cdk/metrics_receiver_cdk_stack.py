7d31f67ca9d1367e6fb6dae28297c397a754185d
Diago Estrada <estradadiago93@gmail.com>
2023-04-21 11:41:46 -0400

    Add SQS queue for device messages

--- metrics_receiver_cdk/metrics_receiver_cdk_stack.py
+++ metrics_receiver_cdk/metrics_receiver_cdk_stack.py
@@ -1,13 +1,20 @@
 from aws_cdk import (
-    Stack
+    Stack,
+    Duration,
+    aws_sqs as sqs,
 )
 from constructs import Construct
 
 
 class MetricsReceiverCdkStack(Stack):
-    def __init__(self, scope: Construct, construct_id: str, prefix: str, environment: str, config: dict, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, prefix: str, environment: str, config: dict,
+                 **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         self.prefix = prefix
         self.suffix = environment.capitalize()
         self.configuration = config
+
+        queue = sqs.Queue(self, f"{self.prefix}Queue{self.suffix}",
+                          queue_name=f"{self.prefix}Queue{self.suffix}",
+                          visibility_timeout=Duration.minutes(15))
