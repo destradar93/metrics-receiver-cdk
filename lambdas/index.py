f50ea436ca0d75b6bf28409ee0ad511e140a9cc4
Diago Estrada <estradadiago93@gmail.com>
2023-04-21 16:08:23 -0400

    Convert float values to Decimal types for DynamoDB insertion

--- lambdas/index.py
+++ lambdas/index.py
@@ -1,5 +1,6 @@
 import json
 import os
+from decimal import Decimal
 
 import boto3
 
@@ -12,5 +13,10 @@ def lambda_handler(event, context):
 
     for record in event['Records']:
         payload = json.loads(record['body'])
+
+        # Convert any float values in the payload to Decimal types
+        for key, value in payload.items():
+            if isinstance(value, float):
+                payload[key] = Decimal(str(value))
 
         table.put_item(Item={"deviceId": payload['deviceId'], "timestamp": payload['timestamp'], "payload": payload})
